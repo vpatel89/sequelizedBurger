5633f7f912defbfd3f526475f721b0ae9fee19df
Viren Patel <virenpatel89@gmail.com>
2016-10-11 02:51:34 -0400

    fixed hw

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,16 @@
-var orm = require('../config/orm.js');
-
-var burger = {
-	all: function(cb) {
-		orm.all('burgers', function(res){
-			cb(res)
-		});
-	},
-	create: function(name,cb) {
-		orm.create('burgers', ['burger_name', 'devoured'], [name, false], cb);
-	},
-	update: function(id, cb) {
-		var condition = 'id=' + id;
-		orm.update('burgers', {devoured : true}, condition, cb);
-	}
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define('Burger', {
+    burger_name: DataTypes.STRING,
+    devoured: {
+    	type: DataTypes.BOOLEAN,
+    	defaultValue: false // only line we needed to add.
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Burger;
 };
-
-module.exports = burger;
